4368b80283fc075dd996ccd4228a7e323a7e99b6
Kaushik-C <32328255+Kaushik-C@users.noreply.github.com>
2020-07-20 12:04:35 +0200

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,45 @@
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
+bool bpmIsOk(float bpm)
+{
+  bool retVal = false;
+  if(bpm > 70 || bpm < 150)
+  {
+    retVal = true;
   }
-  return true;
+  return retVal;  
+}
+
+bool spo2IsOk(float spo2)
+{
+  bool retVal = false;
+  if(spo2 > 80)
+  {
+    retVal = true;
+  }
+  return retVal;  
+}
+
+bool respRateIsOk(float respRate)
+{
+  bool retVal = false;
+  if(respRate > 30 || respRate < 60)
+  {
+    retVal = true;
+  }
+  return retVal;  
+}
+
+
+bool vitalsAreOk(float bpm, float spo2, float respRate)
+{
+  bool retVal = false;
+  if((bpmIsOk(bpm) == false) || (spo2IsOk(spo2) == false) || (respRateIsOk(respRate) == false))
+  {
+     retVal = false;
+  }
+  else
+  {
+    retVal = true;
+  }
+  return retVal;
 }
