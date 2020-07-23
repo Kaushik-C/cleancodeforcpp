c0b3e9735c8bc813f4b8f4997fbf1cf1a8abfef7
Kaushik-C <32328255+Kaushik-C@users.noreply.github.com>
2020-07-23 07:22:42 +0200

    Update paramchecker.cpp
    Vector concept has been used to satisfy open-close principle

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,26 +1,39 @@
-bool checkbpm(float bpm)
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "paramchecker.h"
+
+using namespace std;
+
+std::vector<VitalsAreOk::Vitals_input> input;
+
+static const std::map<VitalsAreOk::VitalNames, VitalsAreOk::Vitals_BoundaryValues> input = {
+	{ VitalsAreOk::VitalNames::bpm,{ 70, 150 } },
+	{ VitalsAreOk::VitalNames::spo2,{ 80, 101 } },
+	{ VitalsAreOk::VitalNames::respRate,{ 30, 60 } }
+};
+
+
+
+bool VitalsAreOk::CheckVitals(int  VitalID, float actual_value)
 {
-  bool retVal;
-  
-  retVal = (bpm > 70 && bpm < 150) ? true : false;
- 
-  return retVal;  
+	bool retVal = true;
+	/*Get the name and actual value*/
+	//cin -- not done now
+
+	for (int i = 0; i < input.size;i++)
+	{
+		if (input[i].VitalID == VitalID)
+		{
+			retVal = ((actual_value > input[i].Vital_Boundaries.minValue) && (actual_value < input[i].Vital_Boundaries.maxValue)) ? true : false;
+		}
+	}
+
+	return retVal;
 }
 
-bool checkspo2(float spo2)
-{
-  bool retVal;
-  
-  retVal = (spo2 > 80) ? true : false;
- 
-  return retVal;  
-}
 
-bool checkrespRate(float respRate)
-{
-  bool retVal;
-  
-  retVal = (respRate > 30 && respRate < 60) ? true : false;
- 
-  return retVal;  
-}
+
