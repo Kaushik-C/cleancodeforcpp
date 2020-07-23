34abb25d8cfeb8091c8f738e948311743c702faf
Kaushik-C <32328255+Kaushik-C@users.noreply.github.com>
2020-07-23 07:24:00 +0200

    Vector concept used - Initial step

--- paramchecker.h
+++ paramchecker.h
@@ -1,4 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
 
-bool checkbpm(float bpm);
-bool checkspo2(float spo2);
-bool checkrespRate(float respRate);
+using namespace std;
+
+#ifndef PARAMCHECKER_H
+#define PARAMCHECKER_H
+
+class VitalsAreOk
+{
+public:
+	enum VitalNames
+	{
+		bpm = 0,
+		spo2,
+		respRate
+	};
+	struct Vitals_BoundaryValues
+	{
+		float minValue;
+		float maxValue;
+	};
+
+	struct Vitals_input
+	{
+		VitalNames VitalID;
+		Vitals_BoundaryValues Vital_Boundaries;
+	};
+
+	bool CheckVitals(int VitalID, float actual_value);
+
+private:
+
+};
+
+#endif //PARAMCHECKER_H
+
